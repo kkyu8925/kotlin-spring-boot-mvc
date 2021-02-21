8879b5972b8a9023f1db148ef4be324ca229350c
gusrb8925 <gusrb8925@naver.com>
2021-02-21 22:15:33 +0900

    21-02-21 JSR-380 Bean Validation

--- src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
@@ -3,7 +3,11 @@ package com.example.mvc.controller.put
 import com.example.mvc.model.http.Result
 import com.example.mvc.model.http.UserRequest
 import com.example.mvc.model.http.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api")
@@ -20,39 +24,52 @@ class PutApiController {
     }
 
     @PutMapping(path = ["/put-mapping/object"])
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
-        // 0. Response
-        return UserResponse().apply {
-            // 1. result
-            this.result = Result().apply {
-                this.resultCode = "OK"
-                this.resultMessage = "성공"
+    fun putMappingObject(@Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult): ResponseEntity<String> {
+
+        if(bindingResult.hasErrors()) {
+            // 500 error
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach{
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field+" : " +message+"\n")
             }
-        }.apply {
-            // 2. description
-            this.description = "~~~~~~"
-        }.apply {
-            // 3. user mutable list
-            val userList = mutableListOf<UserRequest>()
-
-            userList.add(userRequest)
-
-            userList.add(UserRequest().apply {
-                this.name = "a"
-                this.age = 10
-                this.email = "a@gmail.com"
-                this.address = "a address"
-                this.phoneNumber = "010-1111-1111"
-            })
-            userList.add(UserRequest().apply {
-                this.name = "b"
-                this.age = 20
-                this.email = "b@gmail.com"
-                this.address = "b address"
-                this.phoneNumber = "010-2222-2222"
-            })
-
-            this.userRequest = userList
+            return ResponseEntity.badRequest().body(msg.toString())
         }
+        return ResponseEntity.ok("OK")
+
+        // 0. Response
+//        return UserResponse().apply {
+//            // 1. result
+//            this.result = Result().apply {
+//                this.resultCode = "OK"
+//                this.resultMessage = "성공"
+//            }
+//        }.apply {
+//            // 2. description
+//            this.description = "~~~~~~"
+//        }.apply {
+//            // 3. user mutable list
+//            val userList = mutableListOf<UserRequest>()
+//
+//            userList.add(userRequest)
+//
+//            userList.add(UserRequest().apply {
+//                this.name = "a"
+//                this.age = 10
+//                this.email = "a@gmail.com"
+//                this.address = "a address"
+//                this.phoneNumber = "010-1111-1111"
+//            })
+//            userList.add(UserRequest().apply {
+//                this.name = "b"
+//                this.age = 20
+//                this.email = "b@gmail.com"
+//                this.address = "b address"
+//                this.phoneNumber = "010-2222-2222"
+//            })
+//
+//            this.userRequest = userList
+//        }
     }
 }
--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -3,14 +3,41 @@ package com.example.mvc.model.http
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.lang.Exception
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.constraints.*
 
 //@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest (
+
+    @field:NotEmpty
+    @field:Size(min = 2, max = 8)
     var name:String?=null,
+
+    @field:PositiveOrZero // 0 이상의 숫자 (양수)
     var age:Int?=null,
+
+    @field:Email
     var email:String?=null,
+
+    @field:NotBlank
     var address:String?=null,
 
-//    @JsonProperty("phone_number") 
-    var phoneNumber:String?=null // phone_number
-)
+//    @JsonProperty("phone_number")
+    @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$")  // 정규식 검증
+    var phoneNumber:String?=null, // phone_number
+
+    var createAt:String?=null // yyy-MM-dd NN:mm:ss
+){
+    @AssertTrue(message = "생성일자 패턴 yyy-MM-dd NN:mm:ss")
+    private fun isValiCreateAt():Boolean{ // 정상 true, 비정상 false
+
+        return try{
+            LocalDateTime.parse(this.createAt, DateTimeFormatter.ofPattern("yyyy-MM-dd NN:mm:ss"))
+            true
+        } catch (e:Exception) {
+            false
+        }
+    }
+}
